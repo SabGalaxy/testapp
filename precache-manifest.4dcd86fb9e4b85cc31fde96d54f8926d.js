fea2918d13dfebcfd0721add808e8099d05353fa
Sabarish Hariraj <sabarishraj88@gmail.com>
2019-11-26 14:36:47 -0500

    Updates

---
+++ precache-manifest.4dcd86fb9e4b85cc31fde96d54f8926d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508ce5e85a7feb0dd8c0b0a6edc67477",
+    "url": "/SabGalaxy/testapp.git/index.html"
+  },
+  {
+    "revision": "86704b6286a716046e6b",
+    "url": "/SabGalaxy/testapp.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "33641e74fb83a855cc13",
+    "url": "/SabGalaxy/testapp.git/static/js/2.7bc898ea.chunk.js"
+  },
+  {
+    "revision": "86704b6286a716046e6b",
+    "url": "/SabGalaxy/testapp.git/static/js/main.f3631cf5.chunk.js"
+  },
+  {
+    "revision": "89ae5ba9be58cecb02fd",
+    "url": "/SabGalaxy/testapp.git/static/js/runtime-main.a2309371.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/SabGalaxy/testapp.git/static/media/logo.25bf045c.svg"
+  }
+]);
